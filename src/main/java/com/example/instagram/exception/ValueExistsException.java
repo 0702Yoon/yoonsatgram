d5d80fc8cab4848dd43819a9966c099c8fda9fe3
YOON <gkstkddbs99@mju.ac.kr>
2024-02-01 00:05:14 +0900

    feat: 값 존재 유무 예외 처리

---
+++ src/main/java/com/example/instagram/exception/ValueExistsException.java
@@ -0,0 +1,9 @@
+package com.example.instagram.exception;
+
+public class ValueExistsException extends RuntimeException {
+
+    public ValueExistsException(String message) {
+        super(message);
+    }
+
+}
